28a0a869d9dd4e8318d0528eaecd95c4fd361c8e
cunyu1943 <747731461@qq.com>
2022-02-24 14:16:23 +0800

    Site updated: 2022-02-24 14:16:22

---
+++ js/blog-locker.js
@@ -0,0 +1,8 @@
+const btw = new BTWPlugin();
+btw.init({
+    id: 'container',
+    blogId: '15556-1641043359442-147',
+    name: '村雨遥',
+    qrcode: 'https://gitee.com/cunyu1943/images/raw/master/blogpic/wepublic.png',
+    keyword: '村雨',
+});
