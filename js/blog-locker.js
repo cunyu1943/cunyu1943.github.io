20f7260c22012e61cfd3fb98fec5ea3b504e21cf
cunyu1943 <747731461@qq.com>
2022-10-07 16:55:24 +0800

    Site updated: 2022-10-07 16:55:23

--- js/blog-locker.js
+++ js/blog-locker.js
@@ -3,6 +3,6 @@ btw.init({
     id: 'container',
     blogId: '15556-1641043359442-147',
     name: '村雨遥',
-    qrcode: 'https://gitee.com/cunyu1943/images/raw/master/blogpic/wepublic.png',
+    qrcode: 'https://raw.githubusercontent.com/cunyu1943/cunyu1943.github.io/main/imgs/wechatpublic.png',
     keyword: '村雨',
 });
