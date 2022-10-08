67f86af65f8018c00bf664d2aebf9adc3af433c9
cunyu1943 <747731461@qq.com>
2022-10-08 21:35:18 +0800

    Site updated: 2022-10-08 21:35:18

--- js/blog-locker.js
+++ js/blog-locker.js
@@ -3,6 +3,6 @@ btw.init({
     id: 'container',
     blogId: '15556-1641043359442-147',
     name: '村雨遥',
-    qrcode: 'https://cdn.jsdelivr.net/gh/cunyu1943/cunyu1943@main/imgs/wepublic.gif',
+    qrcode: 'https://s1.ax1x.com/2022/10/08/xG7Nmd.png',
     keyword: '村雨',
 });
