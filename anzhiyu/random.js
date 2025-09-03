26766902b097986ae1c58f4e3d1980b984f804cb
cunyu1943 <747731461@qq.com>
2025-09-03 15:28:18 +0000

    ✨ feat: 新增资源&修改状态栏

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["01/0101-aini/","01/0101-aiqinggongyu/","01/0101-haishanggangqinshi/","01/0101-lianhualou/","01/0101-xianyu/","01/0102-jiandaochenxin/"];function toRandomPost(){
+var posts=["01/0101-aini/","01/0101-aiqinggongyu/","01/0101-haishanggangqinshi/","01/0101-lianhualou/","01/0101-xianyu/","01/0102-jiandaochenxin/","01/0103-mengqi30/","01/0105-labubu/","04/0401-dark-mode-night-eye/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
