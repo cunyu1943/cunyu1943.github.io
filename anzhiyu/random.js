6a69a5067c210356373b95639007a28223494e2c
cunyu1943 <747731461@qq.com>
2025-09-03 11:13:57 +0000

    ✨ feat: 新增资源信息
    爱情公寓、海上钢琴师

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["01/0101-aini/","01/0101-lianhualou/","01/0101-xianyu/","01/0102-jiandaochenxin/"];function toRandomPost(){
+var posts=["01/0101-aini/","01/0101-aiqinggongyu/","01/0101-haishanggangqinshi/","01/0101-lianhualou/","01/0101-xianyu/","01/0102-jiandaochenxin/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
